FIX: 1st order push_grad and high-order hessian

interpol/nd.py
@@ -372,8 +372,8 @@ def hess(inp, grid, bound: List[Bound], spline: List[Spline],
     mask = inbounds_mask(extrapolate, grid, shape)
 
     # precompute weights along each dimension
-    weights, grads, hesss, coords, signs = get_weights(grid, bound, spline, shape,
-                                                       grad=True, hess=True)
+    weights, grads, hesss, coords, signs \
+        = get_weights(grid, bound, spline, shape, grad=True, hess=True)
 
     # initialize
     out = torch.zeros([batch, channel, grid.shape[1], dim, dim],
@@ -391,16 +391,17 @@ def hess(inp, grid, bound: List[Bound], spline: List[Spline],
         idx = [c[n] for c, n in zip(coords, nodes)]
         idx = sub2ind_list(idx, shape)
         idx = idx.expand([batch, channel, idx.shape[-1]])
-        out1 = inp.gather(-1, idx)
+        out0 = inp.gather(-1, idx)
 
         # apply sign
         sign0: List[Optional[Tensor]] = [sgn[n] for sgn, n in zip(signs, nodes)]
         sign1: Optional[Tensor] = make_sign(sign0)
         if sign1 is not None:
-            out1 = out1 * sign1
+            out0 = out0 * sign1
 
         for d in range(dim):
             # -- diagonal --
+            out1 = out0.clone()
 
             # apply weights
             for dd, (weight, hess1, n) \
@@ -408,7 +409,7 @@ def hess(inp, grid, bound: List[Bound], spline: List[Spline],
                 if d == dd:
                     hess11 = hess1[n]
                     if hess11 is not None:
-                        out1 *= hess11
+                        out1 = out1 * hess11
                 else:
                     out1 = out1 * weight[n]
 
@@ -417,10 +418,11 @@ def hess(inp, grid, bound: List[Bound], spline: List[Spline],
 
             # -- off diagonal --
             for d2 in range(d+1, dim):
+                out1 = out0.clone()
 
                 # apply weights
-                for dd, (weight, grad1, hess1, n) \
-                        in enumerate(zip(weights, grads, hesss, nodes)):
+                for dd, (weight, grad1, n) \
+                        in enumerate(zip(weights, grads, nodes)):
                     if dd in (d, d2):
                         grad11 = grad1[n]
                         if grad11 is not None:
@@ -433,7 +435,7 @@ def hess(inp, grid, bound: List[Bound], spline: List[Spline],
 
     # out-of-bounds mask
     if mask is not None:
-        out = out * mask.unsqueeze(-1)
+        out = out * mask.unsqueeze(-1).unsqueeze(-1)
 
     # fill lower triangle
     for d in range(dim):

interpol/splines.py
@@ -152,7 +152,7 @@ class Spline:
         x = x.abs()
         if self.order == 2:
             one = torch.ones([1], dtype=x.dtype, device=x.device)
-            return torch.where(x < 0.5, one, -2 * one)
+            return torch.where(x < 0.5, -2 * one, one)
         if self.order == 3:
             return torch.where(x < 1, 3. * x - 2., 2. - x)
         if self.order == 4:
@@ -163,7 +163,7 @@ class Spline:
             h_low = square(x)
             h_low = - h_low * (x * (5./3.) - 3.) - 1.
             h_mid = x * (x * (x * (5./6.) - 9./2.) + 15./2.) - 7./2.
-            h_up = - x * (x * (x/6. - 3./2.) + 9./2.)
+            h_up = 9./2. - x * (x * (x/6. - 3./2.) + 9./2.)
             return torch.where(x < 1, h_low,
                                torch.where(x < 2, h_mid, h_up))
         if self.order == 6: