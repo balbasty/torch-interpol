da85e836e234390c49db8ec6b6b8fae47ed75823
Yael Balbastre <yael.balbastre@gmail.com>
2023-01-03 17:03:09 -0500

    FIX(spline_coeff): options should not be lists

--- interpol/autograd.py
+++ interpol/autograd.py
@@ -251,8 +251,8 @@ class SplineCoeff(torch.autograd.Function):
     @custom_fwd
     def forward(ctx, input, bound, interpolation, dim, inplace):
 
-        bound = bound_to_nitorch(make_list(bound), as_type='int')
-        interpolation = inter_to_nitorch(make_list(interpolation), as_type='int')
+        bound = bound_to_nitorch(make_list(bound)[0], as_type='int')
+        interpolation = inter_to_nitorch(make_list(interpolation)[0], as_type='int')
         opt = (bound, interpolation, dim, inplace)
 
         # Pull
